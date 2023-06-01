82f0e786a4c0faf55dd3b3d9279fc214f8573d4e
vnelee <viennie.lee@utexas.edu>
2023-05-31 23:43:57 -0500

    Implement sql queries

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
+const cors = require('cors');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+app.use(cors());
 
 const db = mysql.createConnection({
   host: process.env.RDS_HOSTNAME,
@@ -10,24 +14,16 @@ const db = mysql.createConnection({
   database: process.env.RDS_DATABASE
 })
 
-// /iotd/:[yyyy-mm-dd] - picture + caption, characters etc for date
-// /iotd - ^ but today (central time?)
-// /iotd?start_date= - list of image entries since start_date
-// /iotd?end_date= - ^ since end_date
-// /iotd?start_date=[]&end_date=[] - date range
-
-test = new Date()
-console.log(test)
-
-const imgHost = ''
-const datePattern = /^\d{4}[\-]\d{2}[\-]\d{2}$/
+const imgHost = 'https://dumpleandfriends-pics.s3.us-east-2.amazonaws.com/';
+const datePattern = /^\d{4}[\-]\d{2}[\-]\d{2}$/;
+const firstDateString = '2023-05-15'; //TODO change this when finalized
 
 const getTodayCentral = () => {
   const dateToday = new Date();
 
   // todayCentralArr is in ['DD', 'MM', 'YYYY'] format.
   // Using en-GB rather than en-US takes care of
-  // adding zeros for single digit month/days.
+  // including leading zeros for single digit month/days.
   const todayCentralArr = dateToday.toLocaleDateString('en-GB', {timeZone: 'America/Chicago'}).split('/');
   const todayCentral = todayCentralArr.reverse().join('-');
   return todayCentral;
@@ -42,35 +38,105 @@ const validateDate = (dateString) => {
 
   const inputDate = new Date(dateString)
 
-  // Is it before the first date, 2023-05-15 TODO change to whatever first date
-  const firstDate = new Date('2023-05-15');
+  // Is it before the first date
+  const firstDate = new Date(firstDateString);
   if(inputDate < firstDate){
     return false;
   }
 
   // Is it after today
+  const todayCentral = getTodayCentral();
+  const todayCentralDate = new Date(todayCentral);
+  if(inputDate > todayCentralDate){
+    return false;
+  }
 
  return true;
 };
 
 app.get('/imgoftheday', (req, res) => {
-  const startDate = req.query.start_date;
-  const endDate = req.query.end_date;
-  console.log(startDate)
-  console.log(endDate)
-  /*
-  if start and end date
-    validate
-    create query string
-    execute query
-  just start date
-    validate; query
-  just end date
-    validate; query
-  today (no query)
-    getTodayCentral
-    query
-  */
+  let startDate = req.query.start_date;
+  let endDate = req.query.end_date;
+
+  if(!startDate && !endDate){
+    console.log('here')
+    const todayDate = getTodayCentral();
+    console.log(todayDate)
+    db.query(`SELECT DATE_FORMAT(dates.date_key, '%Y-%m-%d') as date, CONCAT('${imgHost}', images.url) as url, images.image_caption,
+    JSON_ARRAYAGG(JSON_OBJECT('character_id', characters.character_id, 'character_name', characters.character_name)) as characters
+      FROM dates
+        INNER JOIN images
+          ON images.image_id=dates.image_id
+        INNER JOIN imageCharacter
+          ON images.image_id=imageCharacter.image_id
+        INNER JOIN characters
+          ON imageCharacter.character_id=characters.character_id
+      WHERE dates.date_key=?;`,
+    todayDate,
+    (err,result) => {
+      if(err){
+        console.log(err);
+      }
+      console.log(result);
+      res.send(result);
+    })
+  }
+
+  if(!startDate){
+    startDate = firstDateString;
+  }
+  else {
+    if(!startDate.match(datePattern)){
+      return res
+        .status(400)
+        .send('Invalid format. Date must be in YYYY-MM-DD format.')
+    }
+    validDate = validateDate(startDate);
+    if(!validDate){
+      return res
+        .status(400)
+        .send(`Invalid date. Date must be between ${firstDateString} and ${getTodayCentral()}`)
+    }
+  }
+
+  if(!endDate){
+    endDate = getTodayCentral();
+  }
+  else {
+    if(!endDate.match(datePattern)){
+      return res
+        .status(400)
+        .send('Invalid format. Date must be in YYYY-MM-DD format.')
+    }
+    validDate = validateDate(endDate);
+    if(!validDate){
+      return res
+        .status(400)
+        .send(`Invalid date. Date must be between ${firstDateString} and ${getTodayCentral()}`)
+    }
+  }
+
+  db.query(`SELECT DATE_FORMAT(dates.date_key, '%Y-%m-%d') as date, CONCAT('${imgHost}', images.url) as url, images.image_caption,
+  JSON_ARRAYAGG(JSON_OBJECT('character_id', characters.character_id, 'character_name', characters.character_name)) as characters
+    FROM dates
+      INNER JOIN images
+        ON images.image_id=dates.image_id
+      INNER JOIN imageCharacter
+        ON images.image_id=imageCharacter.image_id
+      INNER JOIN characters
+        ON imageCharacter.character_id=characters.character_id
+    WHERE dates.date_key BETWEEN ? AND ?
+    GROUP BY date
+    LIMIT 35;`,
+  [startDate, endDate],
+  (err,result) => {
+    if(err){
+      console.log(err);
+    }
+    console.log(result);
+    res.send(result);
+  })
+
 });
 
 app.get('/imgoftheday/:date', (req, res) => {
@@ -83,17 +149,28 @@ app.get('/imgoftheday/:date', (req, res) => {
   if(!validateDate(dateString)){
     return res
       .status(400)
-      .send('Invalid date. Date must be between [TODO PUT START DATE :)] and [generate today]')
+      .send(`Invalid date. Date must be between ${firstDateString} and ${getTodayCentral()}`)
   }
 
-  const {date} = dateString;
-  const sqlImgDateGet = 'SELECT * FROM images WHERE date_key=?';
+  db.query(`SELECT DATE_FORMAT(dates.date_key, '%Y-%m-%d') as date, CONCAT('${imgHost}', images.url) as url, images.image_caption,
+  JSON_ARRAYAGG(JSON_OBJECT('character_id', characters.character_id, 'character_name', characters.character_name)) as characters
+    FROM dates
+      INNER JOIN images
+        ON images.image_id=dates.image_id
+      INNER JOIN imageCharacter
+        ON images.image_id=imageCharacter.image_id
+      INNER JOIN characters
+        ON imageCharacter.character_id=characters.character_id
+    WHERE dates.date_key=?;`,
+  dateString,
+  (err,result) => {
+    if(err){
+      console.log(err);
+    }
+    console.log(result);
+    res.send(result);
+  })
 });
-
-app.get('/images/:id', () => {
-  const id = req.params.id;
-  const sqlImgIdGet = 'SELECT * FROM images WHERE image_id=?';
-})
 
 app.listen(3000, () => {
   console.log('running');
